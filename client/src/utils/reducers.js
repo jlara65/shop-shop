86428ea8dde40cc5275798c1fa553a08183631d6
Joseph Lara <vipercar233@gmail.comgit config --global user.name Joseph>
2022-12-07 02:28:16 -0600

    added cart item

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -47,7 +47,7 @@ export const reducer = (state, action) => {
 
     case REMOVE_FROM_CART:
       let newState = state.cart.filter((product) => {
-        return product._id != action._id;
+        return product._id !== action._id;
       });
 
       return {
